b6ad469ef8494deca31daeb101030341298ec658
synewmark <76981619+synewmark@users.noreply.github.com>
2022-06-14 23:22:58 -0400

    Created test cradle

--- src/main/java/test_cradle/Tester.java
+++ src/main/java/test_cradle/Tester.java
@@ -1,11 +1,12 @@
 package test_cradle;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import game_of_life.GameOfLifeAbstract;
 import game_of_life.GameOfLifeProactive;
@@ -15,34 +16,57 @@ public class Tester {
 	final char dead;
 	final int genlength;
 
-	Tester(String start, String end) {
-		String[] splitArray = start.split("\\r?\\n");
+	final boolean[][] initial;
+	final boolean[][] expected;
+
+	Tester(File start, File end) throws FileNotFoundException {
+		Scanner r = new Scanner(new FileReader(start));
 		int x = -1;
 		int y = -1;
-		String[] header = splitArray[0].split(" ");
+		String firstLine = r.nextLine();
+		String[] header = firstLine.split(" ");
 		x = Integer.parseInt(header[1]);
 		y = Integer.parseInt(header[2]);
+		System.out.println(x + " " + y);
 		genlength = Integer.parseInt(header[0]);
 		alive = header[3].charAt(0);
 		dead = header[4].charAt(0);
-		boolean[][] array = new boolean[x][y];
+		initial = new boolean[x][y];
 		for (int i = 0; i < x; i++) {
+			String line = r.nextLine();
 			for (int j = 0; j < y; j++) {
-				array[i][j] = splitArray[i + 1].charAt(j) == alive;
+				initial[i][j] = line.charAt(j) == alive;
 			}
 		}
-		String[] expectedValues = end.split("\\r?\\n");
-		boolean[][] expected = new boolean[x][y];
+		r.close();
+		Scanner s = new Scanner(new FileReader(end));
+		expected = new boolean[x][y];
 		for (int i = 0; i < x; i++) {
+			String line = s.nextLine();
 			for (int j = 0; j < y; j++) {
-				expected[i][j] = expectedValues[i].charAt(j) == alive;
+				expected[i][j] = line.charAt(j) == alive;
 			}
 		}
-		GameOfLifeAbstract game = new GameOfLifeProactive(array);
-		boolean[][] result = game.getNGeneration(genlength);
-//		System.out.println(printer(result));
-		System.out.println(Arrays.deepEquals(expected, result));
 
+	}
+
+	long run() {
+		GameOfLifeAbstract game = new GameOfLifeProactive(initial);
+		long start = System.nanoTime();
+		boolean[][] result = game.getNGeneration(genlength);
+		long end = System.nanoTime();
+		if (!Arrays.deepEquals(result, expected)) {
+			try {
+				PrintWriter out = new PrintWriter("output.txt");
+				out.print(printer(result));
+				out.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Failed");
+		}
+		return end - start;
 	}
 
 	String printer(boolean[][] array) {
@@ -57,12 +81,35 @@ public class Tester {
 	}
 
 	public static void main(String... args) throws IOException {
-		Path simple = new File("C:\\Users\\ahome\\git\\Optimized_Game_Of_Life\\test_cases\\simple.txt").toPath();
-		Path simpleexp = new File("C:\\Users\\ahome\\git\\Optimized_Game_Of_Life\\test_cases\\simple_expected.txt")
-				.toPath();
-		String content = Files.readString(simple, StandardCharsets.US_ASCII);
-		String content1 = Files.readString(simpleexp, StandardCharsets.US_ASCII);
+		File simple = new File("C:\\Users\\ahome\\git\\Optimized_Game_Of_Life\\test_cases\\large.txt");
+		File simpleexp = new File("C:\\Users\\ahome\\git\\Optimized_Game_Of_Life\\test_cases\\large_expected.txt");
+//		String content = Files.readString(simple, StandardCharsets.US_ASCII);
+//		String content1 = Files.readString(simpleexp, StandardCharsets.US_ASCII);
 //		System.out.println(content);
-		new Tester(content, content1);
+		Tester test = new Tester(simple, simpleexp);
+		System.out.println(test.run());
+//		randomOutput();
+	}
+
+	public static void randomOutput() {
+		try {
+			int x = 20000;
+			int y = 10000;
+			int gen = 1000000;
+			PrintWriter out = new PrintWriter("output.txt");
+			out.println(x + " " + y + " " + gen + " 0 O");
+			for (int i = 0; i < 20000; i++) {
+				for (int j = 0; j < 10000; j++) {
+					out.print(Math.random() < 0.25 ? '0' : 'O');
+				}
+				out.println();
+				System.out.println("Finished line: " + i);
+			}
+			out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
